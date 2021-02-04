e19a0d707e84f5f307f71e1e85e4c1d53cb38f76
SemyonAleksandrov <aleksandrosemnik@gmail.com>
2021-02-04 10:58:15 +0300

    feat: add another solutiion

--- src/index.js
+++ src/index.js
@@ -1,4 +1,9 @@
 module.exports = function towelSort(matrix) {
     if (arguments.length === 0) return [];
-    return matrix.flatMap((arr, index) => (index % 2 ? arr.reverse() : arr));
+    // return matrix.flatMap((arr, index) => (index % 2 ? arr.reverse() : arr));
+
+    //another solution
+    let resultArr = [];
+    matrix.forEach((item,index) => index % 2 === 0 ? resultArr.push(item) : resultArr.push(item.reverse()));
+    return resultArr.flat();
 };
